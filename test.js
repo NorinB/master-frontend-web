a6251ec6a8d363bd9f93f95add29ee2b1873eb35
Noah Bauer <accounts@noahbauer.de>
2024-05-27 22:46:47 +0200

    testing: add and remove member from board

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ async function testWebTransport() {
   // Create a WebTransport instance connecting to the Rust server
   console.log("Starte WebTransport...");
   const certificateArray = new Uint8Array([
-    235, 232, 85, 165, 168, 153, 92, 211, 226, 161, 139, 41, 97, 187, 104, 134,
-    59, 230, 116, 30, 128, 213, 166, 220, 219, 229, 206, 151, 66, 138, 142, 107,
+    185, 147, 37, 47, 6, 78, 93, 118, 153, 201, 252, 17, 101, 177, 12, 226, 238,
+    203, 189, 62, 45, 100, 97, 157, 25, 208, 42, 176, 75, 50, 98, 173,
   ]);
   console.log("Starte WebTransport...");
   let transport = new WebTransport("https://[::1]:3031", {
@@ -14,17 +14,34 @@ async function testWebTransport() {
   await transport.ready;
 
   // Create a bidirectional stream
-  let stream = await transport.createBidirectionalStream();
+  const stream = await transport.createBidirectionalStream();
+  const writer = stream.writable.getWriter();
+  const reader = stream.readable.getReader();
 
-  let testMessageJson = { message_type: "board_memberadded" };
+  let testMessageJson = {
+    message_type: "board_memberadd",
+    boardId: "6654ef80584b12a4910e7f39",
+    userId: "6654b896b687bac73d51e6da",
+  };
 
   // Send data from the client to the server
-  await stream.writable
-    .getWriter()
-    .write(new TextEncoder().encode(JSON.stringify(testMessageJson)));
+  await writer.write(new TextEncoder().encode(JSON.stringify(testMessageJson)));
 
   // Read data reply from the server
-  let data = await stream.readable.getReader().read();
+  let data = await reader.read();
+  console.log(new TextDecoder().decode(data.value));
+
+  testMessageJson = {
+    message_type: "board_memberremove",
+    boardId: "6654ef80584b12a4910e7f39",
+    userId: "6654b896b687bac73d51e6da",
+  };
+
+  // Send data from the client to the server
+  await writer.write(new TextEncoder().encode(JSON.stringify(testMessageJson)));
+
+  // Read data reply from the server
+  data = await reader.read();
   console.log(new TextDecoder().decode(data.value));
 }
 
