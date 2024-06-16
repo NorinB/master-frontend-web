c1a119c1bad8da74dd98838f01b4e5d65d979d03
Noah Bauer <accounts@noahbauer.de>
2024-06-16 02:20:14 +0200

    add home component base

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  works = true;
+
+  changeWorks() {
+    this.works = !this.works;
+  }
+}
