ca8820f787d4686fde50a9dd19b0df59e306dcc8
Noah Bauer <bauer.noah712@gmx.de>
2024-08-13 04:10:26 +0200

    update project with certificate

--- src/app/shared/webtransport/webtransport.service.ts
+++ src/app/shared/webtransport/webtransport.service.ts
@@ -11,7 +11,7 @@ import { Mutex } from 'async-mutex';
 })
 export class WebTransportService {
   private webTransportUrl = environment.webTransportUrl;
-  private webTransportCertificate = environment.webTransportCertificate;
+  // private webTransportCertificate = environment.webTransportCertificate;
   private webTransportClient: WebTransportClient | null = null;
   private webTransportTransport: WebTransportTransport | null = null;
   private boardSendStream: WebTransportSendStream | null = null;
@@ -27,13 +27,34 @@ export class WebTransportService {
     if (!(await this.isClosed())) {
       await this.closeConnection();
     }
-    this.webTransportClient = new WebTransportClient(this.webTransportUrl, this.webTransportCertificate);
+    const certificateBytes = await this.getCertificateBytes();
+    this.webTransportClient = new WebTransportClient(this.webTransportUrl, certificateBytes);
     await this.webTransportClient.init_session();
     this.boardSendStream = await this.webTransportClient.setup_connection('board', boardId);
     this.elementSendStream = await this.webTransportClient.setup_connection('element', boardId);
     this.activeMemberSendStream = await this.webTransportClient.setup_connection('active_member', boardId);
     this.clientSendStream = await this.webTransportClient.setup_connection('client', userId);
     this.webTransportTransport = await this.webTransportClient.get_transport();
+  }
+
+  private getCertificateBytes(): Uint8Array {
+    const cert =
+      'MIIDvzCCA0WgAwIBAgISA018LqpnDSGM/b6ddlKRgljMMAoGCCqGSM49BAMDMDIxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MQswCQYDVQQDEwJFNTAeFw0yNDA4MTIyMjQ0NTNaFw0yNDExMTAyMjQ0NTJaMCsxKTAnBgNVBAMTIG1hc3Rlci13ZWJ0cmFuc3BvcnQubm9haGJhdWVyLmRlMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEKEhTdheEqAPpSos2SvZTicScuXS+lou8v9E1pHNPW4Zl1ebDtEuMJM+QF4qM2tE18malSkcaHIK688Dg2UALqC2tx191axhxZ9fLeACT8XAtr2KdPpbLrIQHBLfwWkmQo4ICIzCCAh8wDgYDVR0PAQH/BAQDAgeAMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBSI0cuIoFcHrFfmKyEN20B+xf1h9jAfBgNVHSMEGDAWgBSfK1/PPCFPnQS37SssxMZwi9LXDTBVBggrBgEFBQcBAQRJMEcwIQYIKwYBBQUHMAGGFWh0dHA6Ly9lNS5vLmxlbmNyLm9yZzAiBggrBgEFBQcwAoYWaHR0cDovL2U1LmkubGVuY3Iub3JnLzArBgNVHREEJDAigiBtYXN0ZXItd2VidHJhbnNwb3J0Lm5vYWhiYXVlci5kZTATBgNVHSAEDDAKMAgGBmeBDAECATCCAQUGCisGAQQB1nkCBAIEgfYEgfMA8QB2ABmYEHEJ8NZSLjCA0p4/ZLuDbijM+Q9Sju7fzko/FrTKAAABkUj5nPYAAAQDAEcwRQIhAJB5oQWvmH+FWBMZK7LHcxntgzK1qSdq03t5EtvsetIhAiAeEdBtX4TnRoqOGbcD8wWTnaUeFQDz+Grttq87h3BGUAB3AO7N0GTV2xrOxVy3nbTNE6Iyh0Z8vOzew1FIWUZxH7WbAAABkUj5nKwAAAQDAEgwRgIhAMc/bOqjWFXRUIeopv5MMsZAsa4y3+085ZYD1z+nBvIUAiEAt/iFsfE4d7Xp4qKKadSmnxuwHjHSu7z+PhiXuGIPlf0wCgYIKoZIzj0EAwMDaAAwZQIxAPJqs8Qe1L0sLze9YXXyYw2y7luBHFD2tJA3jBQSW8bgAQKl0+oWvIQkIwGCYN//aAIwPqiWWLQWc4nlLpOEKMBqdxdUp4uE3hnEbGOCqdedmhWhSw2Gerjm9JhTRJw0Sxid';
+    const pemHeader = '-----BEGIN CERTIFICATE REQUEST-----';
+    const pemFooter = '-----END CERTIFICATE REQUEST-----';
+    console.log(cert);
+    const pem = cert.replace(pemHeader, '').replace(pemFooter, '').replace(/\s/g, '');
+    const binaryString = atob(pem);
+    const binaryLen = binaryString.length;
+    const bytes = new Uint8Array(binaryLen);
+    for (let i = 0; i < binaryLen; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    console.log('Länge der Bytes: ', bytes.length);
+    bytes;
+    return new Uint8Array([
+      90, 242, 129, 206, 115, 222, 118, 21, 220, 230, 195, 113, 41, 68, 34, 24, 105, 94, 86, 89, 104, 78, 20, 162, 3, 193, 202, 102, 126, 209, 79, 93,
+    ]);
   }
 
   public async isClosed(): Promise<boolean> {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://master-backend.noahbauer.de',
-  webTransportUrl: 'https://master-backend-wt.noahbauer.de',
+  apiBaseUrl: 'http://master.noahbauer.de',
+  webTransportUrl: 'https://master-webtransport.noahbauer.de',
   webTransportCertificate: new Uint8Array([
     87, 234, 27, 22, 147, 171, 105, 111, 178, 129, 38, 23, 88, 13, 27, 127, 185, 153, 73, 129, 248, 125, 253, 37, 168, 202, 43, 69, 178, 164, 21, 172,
   ]),
