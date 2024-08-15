0974cd90310c97e21478a6bf39588daaf7806d35
Noah Bauer <bauer.noah712@gmx.de>
2024-08-15 13:24:14 +0200

    add correct certificate

--- test.js
+++ test.js
@@ -3,6 +3,65 @@ var certificate = [
   225, 166, 205, 37, 214, 104, 250, 122, 172, 255, 62, 95, 54, 215, 176, 25, 26, 41, 67, 16, 126, 19, 127, 103, 181, 146, 161, 210, 27, 130, 85, 91,
 ];
 
+async function testWebTransportWithDomain() {
+  // Create a WebTransport instance connecting to the Rust server
+  const certificateArray = new Uint8Array(certificate);
+  console.log('Starte WebTransport...');
+  let transport = new WebTransport('https://192.168.178.21:3031', {
+    serverCertificateHashes: [{ algorithm: 'sha-256', value: certificateArray.buffer }],
+  });
+  await transport.ready;
+
+  // Create a bidirectional stream
+  const stream = await transport.createBidirectionalStream();
+  const writer = stream.writable.getWriter();
+  const reader = stream.readable.getReader();
+
+  const initMessage = {
+    messageType: 'init',
+    eventCategory: 'board',
+    contextId: boardId,
+  };
+
+  console.log('Before Write a mesage');
+
+  // Send data from the client to the server
+  await writer.write(new TextEncoder().encode(JSON.stringify(initMessage)));
+
+  console.log('After Write a mesage');
+  // Read data reply from the server
+  let data = await reader.read();
+  console.log(new TextDecoder().decode(data.value));
+  //
+  // const boardAddedMessage = {
+  //   message_type: 'board_memberadd',
+  //   boardId: boardId,
+  //   userId: '6654b896b687bac73d51e6da',
+  // };
+  //
+  // // Send data from the client to the server
+  // await writer.write(new TextEncoder().encode(JSON.stringify(boardAddedMessage)));
+  //
+  // // Read data reply from the server
+  // data = await reader.read();
+  // console.log(new TextDecoder().decode(data.value));
+  //
+  // const boardRemovedJson = {
+  //   message_type: 'board_memberremove',
+  //   boardId: boardId,
+  //   userId: '6654b896b687bac73d51e6da',
+  // };
+  //
+  // // Send data from the client to the server
+  // await writer.write(new TextEncoder().encode(JSON.stringify(boardRemovedJson)));
+  //
+  // // Read data reply from the server
+  // data = await reader.read();
+  // console.log(new TextDecoder().decode(data.value));
+
+  transport.close();
+}
+
 async function testWebTransport() {
   // Create a WebTransport instance connecting to the Rust server
   const certificateArray = new Uint8Array(certificate);
