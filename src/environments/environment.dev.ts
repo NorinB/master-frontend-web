fe046ca1916d1fbf63747fc2b2b4363beec8ed1c
Noah Bauer <bauer.noah712@gmx.de>
2024-10-03 20:33:37 +0200

    [REFAC] add new certificate

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://192.168.178.31:3030',
-  webTransportUrl: 'https://192.168.178.31:3031',
+  apiBaseUrl: 'http://localhost:3030',
+  webTransportUrl: 'https://localhost:3031',
   webTransportCertificate: new Uint8Array([
-    87, 234, 27, 22, 147, 171, 105, 111, 178, 129, 38, 23, 88, 13, 27, 127, 185, 153, 73, 129, 248, 125, 253, 37, 168, 202, 43, 69, 178, 164, 21, 172,
+    158, 176, 242, 43, 66, 45, 182, 47, 11, 124, 65, 223, 84, 236, 230, 51, 57, 51, 182, 147, 227, 184, 216, 192, 166, 170, 172, 85, 200, 171, 15, 202,
   ]),
 };
